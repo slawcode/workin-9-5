69ad2a7eab2751d67a53eaed20d77d8aa1f46b20
Sheryl L <s_lawrence0@yahoo.ca>
2024-02-05 13:47:11 -0500

    code work

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,12 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var rootEl = $('#root');
+
+import dayjs from 'dayjs'
+
 $(function () {
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +25,10 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+
+  var currentDate = dayjs();
+  var formattedDate = currentDate.format('YYYY-MM-DD HH:mm:ss');
+  console.log(formattedDate); // free code camp
+
+  const currentDate = new Date().toDateString();  // She codes
 });
